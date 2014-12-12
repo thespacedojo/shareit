bb100f8b76f55f2c4475b9e7801437f1f93bb5cf
Josh Owens <joshua.owens@gmail.com>
2014-12-11 23:01:52 -0500

    Update github link.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  summary: 'A meteorite package that makes social sharing easy',
-  git: 'https://github.com/Differential/shareit',
+  summary: 'A meteor package that makes social sharing easy',
+  git: 'https://github.com/meteorclub/shareit',
   version: '0.3.0',
   name: "joshowens:shareit"
 });
